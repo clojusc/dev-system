2b895789161ced306f530b7f5e6d32e0cefaaefe
Duncan McGreggor <oubiwann@gmail.com>
2018-07-27 10:15:36 -0500

    Added docstrings and comments.

--- dev-resources/src/clojusc/system_manager/repl.clj
+++ dev-resources/src/clojusc/system_manager/repl.clj
@@ -21,14 +21,25 @@
 
 (defn init
   []
+  "This is used to set the options and any other global data.
+
+  This is defined in a function for re-use. For instance, when a REPL is
+  reloaded, the options will be lost and need to be re-applied."
   (logger/set-level! '[clojusc.dev] :debug)
   (setup-manager setup-options))
 
 (defn init-and-startup
   []
+  "This is used as the 'after-refresh' function by the REPL tools library.
+  Not only do the options (and other global operations) need to be re-applied,
+  the system also needs to be started up, once these options have be set up."
   (init)
   (startup))
 
+;; It is not always desired that a system be started up upon REPL loading.
+;; Thus, we set the options and perform any global operations with init,
+;; and let the user determine when then want to bring up (a potentially
+;; computationally intensive) system.
 (init)
 
 (defn banner
