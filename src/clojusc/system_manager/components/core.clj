7b22da59221c132153d7a6660beeaaa2652cd938
Duncan McGreggor <oubiwann@gmail.com>
2018-07-27 10:08:20 -0500

    Added better error handling and more convenience wrappers to top-level API.

--- src/clojusc/system_manager/components/core.clj
+++ src/clojusc/system_manager/components/core.clj
@@ -1,9 +1,21 @@
 (ns clojusc.system-manager.components.core
-  "A placeholder, no-op component namespace.")
+  "A placeholder, no-op component namespace."
+  (:require
+    [com.stuartsierra.component :as component]))
+
+(defrecord PlaceholderComponent []
+  component/Lifecycle
+
+  (start [component]
+    component)
+
+  (stop [component]
+    component))
 
 (defn init
   "In a real component.core namespace, the init function would be responsible
   for selecting the appropriate function for generating a Component library
   system map."
   []
-  {})
+  (component/system-map
+    :placeholder (map->PlaceholderComponent {})))
